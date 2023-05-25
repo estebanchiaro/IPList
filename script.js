c85d76e3d4be648523e632aafe4354e79638b1e4
estebanchiaro <90053650+estebanchiaro@users.noreply.github.com>
2023-05-25 09:19:56 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let label1= document.querySelector('label1')
+let label1div= document.querySelector('label1div')
+
+label1.addEventListener('click', ()=>{
+    if(label1div.style.display==='none'){
+        label1div.style.display="block"
+    }
+    else{
+        label1div.style.display==='none'
+    }
+
+})
